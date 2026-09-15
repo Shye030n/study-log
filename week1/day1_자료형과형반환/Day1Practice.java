9030a97266e386c5413b2c4cbd872a1aa7e83bc0
Shye030n <shyeo_on@naver.com>
2026-09-15 16:55:21 +0900

    day1: 연습(score, name) 코드 추가

--- week1/day1_자료형과형반환/Day1Practice.java
+++ week1/day1_자료형과형반환/Day1Practice.java
@@ -1,5 +1,6 @@
 public class Day1Practice {
     public static void main(String[] args) {
+        //실습 문제
         int age = 26;
         double price = 4500.5;
         char grade = 'A';
@@ -13,5 +14,14 @@ public class Day1Practice {
 
         System.out.println("ageAsDouble : " + ageAsDouble);
         System.out.println("priceAsInt : " + priceAsInt);
+
+        //연습 문제
+        int score = 87;
+        String name = "김서현";
+
+        double scoreAsDouble = score;
+
+        System.out.println("이름: " + name + ", 점수 : " + score);
+
     }
 }
