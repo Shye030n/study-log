e0e8af0b2f24eeb85a46c30b641534e764fd075b
Shye030n <shyeo_on@naver.com>
2026-09-16 18:17:40 +0900

    day2: switch문 실습 완료 - break 유무에 따른 fall-through 동작 확인

--- week1/day2_연산자와조건문/Day2Practice.java
+++ week1/day2_연산자와조건문/Day2Practice.java
@@ -92,5 +92,32 @@ public class Day2Practice {
         System.out.println("e++ = " + e++);
         System.out.println("e = " + e);
     
+
+        System.out.println("===== switch 실습 =====");
+        int month = 8;
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("겨울");
+                //break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("봄");
+                //break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("여름");
+                //break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("가을");
+                //break;
+            default : System.out.println("잘못된 월");
+        }
+        
     }
 }
