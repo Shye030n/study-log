b0a968513d766d58509bc3f9da38fd574115b355
Shye030n <shyeo_on@naver.com>
2026-09-20 19:37:19 +0900

    day5: String 불변성, substring, split, trim, replace, equals/== 실습 완료

--- week1/day5_String메서드/Day5Practice.java
+++ week1/day5_String메서드/Day5Practice.java
@@ -13,6 +13,49 @@ public class Day5Practice {
         String 타입의 문자열을 바꾸고 싶을 때는, 
         메서드 사용 시 꼭 새로운 변수에 값을 넣어줘야 한다.
         */
+
+        //String 메서드() 실습
+        System.out.println("===== .substring() =====");
+        String fullName = "김서현입니다";
+        String sub1 = fullName.substring(0,3);
+        System.out.println(sub1);
+
+        String sub2 = fullName.substring(4);
+        System.out.println(sub2);
+        System.out.println("sub3 : " + fullName.substring(3));
+
+        System.out.println("===== .split() =====");
+        String csv = "사과,바나나,포도";
+
+        String[] furits = csv.split(",");
+        System.out.println("split로 생성한 배열 총 길이" + furits.length);
+        for (String fruit : furits) {
+            System.out.println(fruit);
+        }
+
+
+        System.out.println("===== .trim() =====");
+        String withSpace = "  안 녕 하 세    요 ";
+        System.out.println("trim 후 : [" + withSpace.trim() + "]");
+        System.out.println("trim 전 : [" + withSpace + "]");
+        
+        System.out.println("----- 전체 공백을 없애려면 ----- replace 사용! ");
+        System.out.println("===== .replace() =====");
+        String noSpaceAll = withSpace.replace(" ", "");
+        System.out.println("replace로 문자열 전체 공백 제거: [" + noSpaceAll + "]");
+        System.out.println("존댓말 변경 : [" + noSpaceAll.replace("세요", "십니까") +"]");
+        
+        System.out.println("===== .equals() vs ==    =====");
+        String a = new String("hello");
+        String b = new String("hello");
+        String c = "hello";
+        String d = "hello";
+
+        System.out.println("a == b : " + (a == b));     //false
+        System.out.println("a.equals(b): " + a.equals(b));     //true
+
+        System.out.println("c == d : " + (c == d));      //false
+        
         
         
 
